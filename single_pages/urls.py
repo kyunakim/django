4918f01ca9e887861ba33afbc42d82616d339bc8
kyunakim <kyunacloud@gmail.com>
2022-08-05 12:28:14 +0900

    up

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('about_me/', views.about_me),
-    path('', views.landing),
+    path('', views.landing), # 빈상태일 때 landing
 ]
--- single_pages/views.py
+++ single_pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+# single_pages 보여주기
 def landing(request):
     return render(
         request,
