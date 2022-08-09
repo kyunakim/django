0b97b1b6f4abd4629577b8da3960f69674351a48
kyunakim <kyunacloud@gmail.com>
2022-08-09 21:59:13 +0900

    up

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,17 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'categories'
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+    
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -30,6 +41,8 @@ class Post(models.Model):
     
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
+    tags = models.ManyToManyField(Tag, blank=True)
+
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
 
@@ -41,4 +54,5 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
 
