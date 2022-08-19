16d5040c2ca7910cefca55461a14bf475153b1ce
kyunakim <kyunacloud@gmail.com>
2022-08-19 11:26:56 +0900

    up

--- dj/settings.py
+++ dj/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'blog',
     'single_pages',
     'program',
+    'ml',
 ]
 
 AUTHENTICATION_BACKENDS = (
---
+++ ml/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+import joblib
+import pandas as pd
+import pickle
+
+# Create your views here.
+def inputdata(request):
+    return render(request,'ml/inputdata.html')
+
+def ml_result(request):
+    cls = joblib.load('tcl_model.pkl')
+
+    df = pd.DataFrame(columns=['fare_cat', 'age_cat', 'family', 'sex_female', 'sex_male',
+        'embarked_C', 'embarked_Q', 'embarked_S'])
+    
+    lis = []
+
+    lis.append(request.GET['fare_cat'])
+    lis.append(request.GET['age_cat'])
+    lis.append(request.GET['family'])
+    lis.append(request.GET['sex_female'])
+    lis.append(request.GET['sex_male'])
+    lis.append(request.GET['embarked_C'])
+    lis.append(request.GET['embarked_Q'])
+    lis.append(request.GET['embarked_S'])
+
+    df.loc[0,:] = lis
+    ans = cls.predict(df)
+
+    if ans == 0:
+        ans = "Dead"
+    else:
+        ans = "Survived"
+    return render(request, "ml/ml_result.html", {'lis':lis, 'ans':ans})
